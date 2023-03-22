30534a28a0a0a29e7a38fb897590f103cc6e8e8c
IvanKulik <127328617+IvanKulik@users.noreply.github.com>
2023-03-22 08:36:19 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+
+function but1() {
+    let text = document.querySelector('input');
+    text.disabled = true;
+}
+
+function but2() {
+    let text = document.querySelector('input');
+    text.disabled = false;
+}
+function but3() {
+    let el = document.getElementById('example');
+    if (el.hidden != true) {
+        document.getElementById('example').hidden = true;
+        document.getElementById('Button').innerText = 'Показать квадрат';
+        return;
+    }
+    document.getElementById('example').hidden = false;
+    document.getElementById('Button').innerText = 'Скрыть квадрат';
+}
+
+
+
+
+let text = document.querySelectorAll('.red');
+let arr = Array.prototype.map.call(text, function (el, i) {
+    return el.id;
+});
+
+for (let i = 0; i < text.length; i++) {
+
+    document.getElementById(arr[i]).onclick = function () {
+        arr.forEach((element, index, Array) => {
+            document.getElementById(element).style.backgroundColor = 'red';
+        });
+        text[i].style.backgroundColor = "green";
+
+    };
+}
